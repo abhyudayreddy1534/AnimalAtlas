eebb6990147396c5ce2be1fea3aa53eb06ef4af7
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-02-16 12:02:46 -0600

    Initial Commit

---
+++ AnimalAtlas/AnimalAtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalAtlasApp.swift
+//  AnimalAtlas
+//
+//  Created by Sravanthi Chinthireddy on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimalAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
